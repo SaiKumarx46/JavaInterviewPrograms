81aa623305b827c3b9f526865f2d6e36a20a8b57
SAI KUMAR P <144097049+SaiKumarx46@users.noreply.github.com>
2023-09-06 16:35:37 +0530

    Add files via upload

---
+++ 2.Swap_without_third_variable/Swap_without_third.java
@@ -0,0 +1,13 @@
+
+public class Swap_without_third {
+    public static void main(String[] args) {
+        int a=10;
+        int b=5;
+        a=a+b;
+        System.out.println(a);
+        b=a-b;
+        a=a-b;
+        System.out.println(a+" "+b);
+        
+    }
+}
